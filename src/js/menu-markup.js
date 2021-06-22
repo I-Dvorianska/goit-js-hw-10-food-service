abd90663600a94f9dfe419efa8e360cc26eda93c
Alex <alexmatvichuk@gmail.com>
2021-06-22 21:45:01 +0200

    inserted menu items in html

--- src/js/menu-markup.js
+++ src/js/menu-markup.js
@@ -1,4 +1,8 @@
 import template from '../templates/menu-items.hbs';
 import menuItems from '../menu.json';
 
-console.log(template(menuItems));
+const menuList = document.querySelector('.menu');
+
+const createMenuMarkup = template(menuItems);
+
+menuList.insertAdjacentHTML('beforeend', createMenuMarkup);
