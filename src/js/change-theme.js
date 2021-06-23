0e9e5f14ca08241a90d73d67d0dcbe13f91642ae
Alex <alexmatvichuk@gmail.com>
2021-06-23 17:48:46 +0200

    add theme changes

---
+++ src/js/change-theme.js
@@ -0,0 +1,33 @@
+const switchToggle = document.querySelector('#theme-switch-toggle');
+const indexBody = document.querySelector('body');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+switchToggle.addEventListener('change', changeBodyTheme);
+
+function changeBodyTheme(e) {
+  e.preventDefault();
+
+  if (e.target.checked) {
+    removeClassFromBody(Theme.LIGHT);
+    addClassForBody(Theme.DARK);
+  }
+
+  if (!e.target.checked) {
+    if (indexBody.classList.contains(Theme.DARK)) {
+      removeClassFromBody(Theme.DARK);
+    }
+    addClassForBody(Theme.LIGHT);
+  }
+}
+
+function addClassForBody(theme) {
+  indexBody.classList.add(theme);
+}
+
+function removeClassFromBody(theme) {
+  indexBody.classList.remove(theme);
+}
